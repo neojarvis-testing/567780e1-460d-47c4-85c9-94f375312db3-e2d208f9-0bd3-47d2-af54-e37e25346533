520e326379ff58ff648ccd7cc29670f804bf5d3e
Harshsarwade <harshsarwade2002@gmail.com>
2025-04-26 06:32:28 +0000

    models

---
+++ dotnetapp/Models/Feedback.cs
@@ -0,0 +1,12 @@
+namespace dotnetapp.Models
+{
+    public class Feedback
+    {
+        
+        public int FeedbackId { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public string FeedbackText { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
