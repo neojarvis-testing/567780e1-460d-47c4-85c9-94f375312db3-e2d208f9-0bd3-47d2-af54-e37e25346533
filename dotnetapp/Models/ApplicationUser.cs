7f367d48fa48181b828f9a90f3b1a5ed12b8da56
rrajesh200203 <rajesh200244@gmail.com>
2025-04-26 06:15:42 +0000

    message

--- dotnetapp/Models/ApplicationUser.cs
+++ dotnetapp/Models/ApplicationUser.cs
@@ -1,7 +1,11 @@
+using System.DataAnnotations;
+using System.DataAnnotations.Schema;
+
 namespace dotnetapp.Models
 {
  public class ApplicationUser : IdentityUser
     {
+         [Required]
          public string Name { get; set; }
     }
 }
--- dotnetapp/Models/LoginModel.cs
+++ dotnetapp/Models/LoginModel.cs
@@ -1,10 +1,16 @@
 
+using System.DataAnnotations;
+using System.DataAnnotations.Schema;
+
 // Models/LoginModel.cs
 namespace dotnetapp.Models
 {
      public class LoginModel
     {
+     [Required]
      public string Email { get; set; }
+
+     [Required]
      public string Password { get; set; }
     }
 }
