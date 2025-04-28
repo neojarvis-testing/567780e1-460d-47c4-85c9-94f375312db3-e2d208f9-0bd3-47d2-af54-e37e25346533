43ae733f058573cc61ac50fc5502fe0fae3c442a
Harshsarwade <harshsarwade2002@gmail.com>
2025-04-28 06:19:04 +0000

    models

--- dotnetapp/Models/Feedback.cs
+++ dotnetapp/Models/Feedback.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotaions;
+using System.ComponentModel.DataAnnotaions.Schema;
+
 namespace dotnetapp.Models
 {
     public class Feedback
     {
-        
+        [Key]
         public int FeedbackId { get; set; }
+
+        [Required(ErrorMessage = "User Id is required.")]
+        [ForeignKey("User")]
         public int UserId { get; set; }
-        public User? User { get; set; }
+
+        [Required(ErrorMessage = "Feedback Text is required.")]
+        [StringLength(64,ErrorMessage = "FeedbackText should not exceed 64 characters.")]
         public string FeedbackText { get; set; }
+
+        [Required(ErrorMessage = "Feedback DateTime is Required.")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid Date Format.")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
+
+        [JsonIgnore]
+        public User? User { get; set; }
     }
 }
