def9da561018bf2cba488e458547520778cb8e93
sudhanshu234 <sudhanshukumar4007@gmail.com>
2025-04-28 06:15:53 +0000

    models

--- dotnetapp/Models/MentorshipProgram.cs
+++ dotnetapp/Models/MentorshipProgram.cs
@@ -5,21 +5,35 @@ namespace dotnetapp.Models
 {
     public class MentorshipProgram
     {      
-
+        [Key]
         public int MentorshipProgramId { get; set; }
-        [Required]
+
+        [Required(ErrorMessage="Program Name is required")]
+        [StringLength(128, ErrorMessage = "Program Name should not exceed 128 character.")]
         public string ProgramName { get; set; }
-          [Required]
+
+        [Required(ErrorMessage="Description is required")]
+        [StringLength(1024, ErrorMessage = "Description should not exceed 1024 character.")]
         public string Description { get; set; }
-          [Required]
+
+        [Required(ErrorMessage="Field of Mentorship is required")]
+        [StringLength(64, ErrorMessage = "Field of Mentorship should not exceed 64 character.")]
         public string FieldOfMentorship { get; set; }
-          [Required]
+
+        [Required(ErrorMessage="Duration is required")]
+        [Range(1,int.MaxValue, ErrorMessage = "Duration must be at least one month or more.")]
         public int DurationInMonths { get; set; }
-          [Required]
+
+        [Required(ErrorMessage="Mentor Name is required")]
+        [StringLength(128, ErrorMessage = "Mentor Name should not exceed 128 character.")]
         public string MentorName { get; set; }
-          [Required]
+
+        [Required(ErrorMessage="Experience Level is required")]
+        [StringLength(64, ErrorMessage = "Experience Level should not exceed 64 character.")]
         public string ExperienceLevel { get; set; }
-          [Required]
+
+        [Required(ErrorMessage="Mode of Mentorship is required")]
+        [StringLength(16, ErrorMessage = "Mode of Mentorship should not exceed 16 character.")]
         public string ModeOfMentorship { get; set; } // Online / Offline / Hybrid
         
     }
