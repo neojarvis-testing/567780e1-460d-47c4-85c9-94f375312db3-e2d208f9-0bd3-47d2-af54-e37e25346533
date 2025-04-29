b9da2fc24a7092abcfb490fb1d1913d6b38a07fe
Harshsarwade <harshsarwade2002@gmail.com>
2025-04-29 16:07:37 +0000

    reapplied

--- angularapp/src/app/components/adminnav/adminnav.component.ts
+++ angularapp/src/app/components/adminnav/adminnav.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminnav',
-  templateUrl: './adminnav.component.html',
-  styleUrls: ['./adminnav.component.css']
+  templateUrl: './adminnav.component.html'
 })
-export class AdminnavComponent implements OnInit {
+export class AdminnavComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout(): void {
+    if (confirm('Are you sure you want to logout?')) {
+      this.authService.logout(); // AuthService's logout method will be tested as-is
+      this.router.navigate(['/login']); // Navigation will follow as per the test case
+    }
   }
-
 }
